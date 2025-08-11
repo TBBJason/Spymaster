150ec164fa700b82f642c49cd4077e2212f5e46a
Jason Wu <wujason19@gmail.com>
2025-08-11 13:45:46 -0400

    orienting it

--- pymain.py
+++ pymain.py
@@ -2,24 +2,39 @@ from google.cloud import vision
 import  io # image handling and file operations
 
 client = vision.ImageAnnotatorClient.from_service_account_file('gcp_key.json')
-# print("Client created successfully.")
 
-image_path = 'spymasterGridDiscord.png'
+class extract_text_from_image:
+    def __init__ (self, image_path, client=client):
+        self.client = client
+        self.image_path = image_path
+        self.grid = []
 
-with io.open(image_path, 'rb') as image_file:
-    content = image_file.read() # this reads the image file in binary mode
-
-image = vision.Image(content=content) 
-
-response = client.text_detection(image=image) # this sends the image to the Vision API for text detection
-texts = response.text_annotations # this extracts the detected texts from the response
-# print(texts)
-grid = [] # list to hold all the texts in a grid format
-curr = []
-for text in texts[1:]:
-    curr.append(text.description)
-    if (len(curr) == 5):
-        grid.append(curr)
+    def extract(self):
+        with io.open(self.image_path, 'rb') as image_file:
+            self.content = image_file.read() # this reads the image file in binary mode
+        image = vision.Image(content=self.content) # this creates an Image object with the content
+        self.response = client.text_detection(image=image)
+        return self.response
+        
+    def formulate_grid(self):
+        self.extract()  # Ensure the image content is extracted before processing
+        self.content
         curr = []
+        texts = self.response.text_annotations
+        for text in texts[1:]:
+            curr.append(text.description)
+            if (len(curr) == 5):
+                self.grid.append(curr)
+                curr = []
+        return self
+    
+    def print_grid(self):
+        for row in self.grid:
+            print(row)
 
-print(grid)
+# grid = extract_text_from_image(image_path).formulate_grid()
+# grid.print_grid()
+if __name__ == "__main__":
+    image_path = 'spymasterGridDiscord.png'
+    grid = extract_text_from_image(image_path=image_path).formulate_grid()
+    grid.print_grid()
