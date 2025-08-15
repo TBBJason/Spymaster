bbbd641a6a687a5c1f65736126e1acf135cd61eb
Jason Wu <wujason19@gmail.com>
2025-08-15 18:04:51 -0400

    starting to try the backend, but walking izzy

--- backend/extract_texts.py
+++ backend/extract_texts.py
@@ -2,35 +2,29 @@ from google.cloud import vision
 import os
 import  io # image handling and file operations
 from calculate import calculator    
-from dotenv import load_dotenv, dotenv_values
+
 
 class extract_text_from_image:
-    def __init__ (self, image_path, APIkey):
+    def __init__ (self, APIkey : str):
         self.client = vision.ImageAnnotatorClient.from_service_account_file(APIkey)
-        self.image_path = image_path
         self.grid = []
 
-    def extract(self):
-        with io.open(self.image_path, 'rb') as image_file:
-            self.content = image_file.read() # this reads the image file in binary mode
-        image = vision.Image(content=self.content) # this creates an Image object with the content
-        self.response = self.client.text_detection(image=image)
-        return self.response
+    def extract(self, image_bytes: bytes) -> list:
+        # with io.open(self.image_path, 'rb') as image_file:
+        #     self.content = image_file.read() # this reads the image file in binary mode
+        image = vision.Image(content=image_bytes) # this creates an Image object with the content
+        response = self.client.text_detection(image=image)
+        self._formulate_grid(response)
+        return self.grid
         
-    def formulate_grid(self):
-        self.extract()  # Ensure the image content is extracted before processing
-        self.content
+    def _formulate_grid(self, response):
         curr = []
-        texts = self.response.text_annotations
+        texts = response.text_annotations
         for text in texts[1:]:
             curr.append(text.description)
             if (len(curr) == 5):
                 self.grid.append(curr)
                 curr = []
-        return self
-    
-    def print_grid(self):
-        for row in self.grid:
-            print(row)
+        return 
 
 
--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,54 @@
 import uvicorn
 from extract_texts import extract_text_from_image
+from fastapi import FastAPI, UploadFile, File
 from calculate import calculator    
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import List
+from dotenv import load_dotenv, dotenv_values
+
+# Load environment variables
+load_dotenv()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)   
+
+APIKey = os.getenv("GOOGLE_VISION_API_KEY")
+print(APIKey)
+text_extractor = extract_text_from_image(APIkey=APIKey)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Spymaster API!"}
+
+@app.post("/process-image")
+async def upload_image(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    grid = text_extractor.extract(image_bytes)
+
+    return {
+        "grid": grid,
+        "file": file.filename,
+        "message": "Image processed successfully",
+            }
+
 
 if __name__ == "__main__":
     # logic for extracting text from an image and calculating vectors
     # APIkey = 'gcp_key.json'
     # image_path = 'spymasterGridDiscord.png'
-    # extractor = extract_text_from_image(image_path=image_path, APIkey=APIkey).formulate_grid()
-    # extractor.print_grid()
-    origins = [
-        "http://localhost:3000",
-    ]
-    app = FastAPI()
-
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins, 
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )   
-    test = calculator()
-    test.bestWord(3)
+    # extractor = extract_text_from_image(image_path=image_path, APIkey=APIkey).formulate
+    
+    uvicorn.run(app, host="127.0.0.1", port=8000)
