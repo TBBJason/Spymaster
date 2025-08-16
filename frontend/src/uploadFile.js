aef1800104a79ea3ab0ab3197bacd7702fc15b3e
Jason Wu <wujason19@gmail.com>
2025-08-16 01:49:47 -0400

    pushing for changes

--- frontend/src/uploadFile.js
+++ frontend/src/uploadFile.js
@@ -14,6 +14,7 @@ function UploadFile() {
     const handleUpload = async () => {
         const formData = new FormData();
         formData.append("file", file);
+        
 
         try {
             setIsUploading(true);
